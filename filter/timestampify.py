f34e5bb9a0e352212a2e24f06166e4b1242c049c
Shane R. Spencer <ss668q@att.com>
2017-08-24 21:37:57 +0000

    test

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -7,7 +7,7 @@ import pprint
 
 a = open('/tmp/wut.txt', 'w')
 a.write(pprint.pformat(os.environ, width=10) + os.linesep)
-a.write(pprint.pformat(os.environ, width=10) + os.linesep)
+a.write(pprint.pformat(sys.argv, width=10) + os.linesep)
 a.close()
 
 
