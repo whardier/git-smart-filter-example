e003bed24a94aa62a433c9517f1a932a75847f98
Shane R. Spencer <ss668q@att.com>
2017-08-25 00:36:53 +0000

    test

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -7,6 +7,8 @@ import pprint
 
 pprint.pprint(os.environ, sys.stderr, width=1, indent=2)
 
+sys.stdout.write('thisn:' + repr(os.getpid()) + os.linesep)
+
 for line in sys.stdin.readlines():
     sys.stderr.write(repr([os.getpid(), sys.argv, line.strip()]) + os.linesep)
     sys.stdout.write(line + os.linesep)
