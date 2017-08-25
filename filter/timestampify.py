25fb276fbdd7f91aaf1ecb77dd25cd30231e75b1
Shane R. Spencer <ss668q@att.com>
2017-08-25 00:31:17 +0000

    test

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -5,6 +5,8 @@ import sys
 
 import pprint
 
+pprint.pprint(os.environ, sys.stderr)
+
 for line in sys.stdin.readlines():
     sys.stderr.write(repr([os.getpid(), sys.argv, line.strip()]) + os.linesep)
     sys.stdout.write(line + os.linesep)
