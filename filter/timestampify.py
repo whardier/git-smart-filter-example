6ffbb4936b72a2d776b44f52de84dfe8d32a56fc
Shane R. Spencer <ss668q@att.com>
2017-08-29 16:34:31 +0000

    stuff

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -5,10 +5,8 @@ import sys
 
 import pprint
 
-pprint.pprint(os.environ, sys.stderr, width=1, indent=2)
-
-sys.stdout.write('thisn:' + repr(os.getpid()) + os.linesep)
+pprint.pprint([os.environ, sys.argv], sys.stderr, width=1, indent=2)
 
 for line in sys.stdin.readlines():
-    sys.stderr.write(repr([os.getpid(), sys.argv, line.strip()]) + os.linesep)
-    sys.stdout.write(line + os.linesep)
+    sys.stdout.write(line)
+    sys.stdout.flush()
