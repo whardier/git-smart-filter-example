2ffbf95d3654ee0c2046a70e523661dc0218eb80
Shane R. Spencer <ss668q@att.com>
2017-08-29 16:52:38 +0000

    stuff

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -4,10 +4,16 @@ import os
 import sys
 
 import pprint
+import json
 
 pprint.pprint([os.environ, sys.argv, os.getpid()], stream=sys.stderr)
 
+json.dump(os.environ, open(str(os.getpid()), 'w'), sort_keys=True, indent=2)
+
 sys.stdout.write(str(os.getpid()) + os.linesep)
+
+filter = sys.argv[1]
+filename = sys.argv[2]
 
 for line in sys.stdin.readlines():
     sys.stdout.write(line)
