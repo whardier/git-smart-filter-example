ca43a30ae5066a6f59127bfa40bf191a3c522c90
Shane R. Spencer <ss668q@att.com>
2017-08-29 17:30:52 +0000

    stuff

--- filter/timestampify.py
+++ filter/timestampify.py
@@ -5,12 +5,11 @@
 import os
 import sys
 
+import re
+
 import pprint
-import json
 
 pprint.pprint([os.environ.__dict__, sys.argv, os.getpid()], stream=sys.stderr)
-
-json.dump(os.environ.__dict__, open(str(os.getpid()), 'w'), sort_keys=True, indent=2, default=repr)
 
 sys.stdout.write(str(os.getpid()) + os.linesep)
 
@@ -18,5 +17,28 @@ filter = sys.argv[1]
 filename = sys.argv[2]
 
 for line in sys.stdin.readlines():
+
+    split_str = line.split('TIMESTAMPIFY:')
+    line_str = split_str[0]
+    var_str = split_str[-1].strip()
+
+    vars = var_str.split(':')
+
+    cmd = vars[0]
+
+    #Example EPOCH command to replace a variable with an epoch
+
+    if cmd == 'EPOCH':
+        clean_var = vars[1]
+        if filter == 'CLEAN':
+            smudge_var = ('%.3f' % time.time()).replace('.', '')
+        new_line_str = line_str
+        new_line_str.replace(clean_var, smudge_var)
+
+        sys.stderr.write('DONE:' + line_str + os.linesep)
+        sys.stderr.write('DONE:' + new_line_str + os.linesep)
+
+        sys.stderr.flush()
+
     sys.stdout.write(line)
     sys.stdout.flush()
